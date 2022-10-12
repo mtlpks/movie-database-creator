bf99fbaa338e7af59eca0038af29d240573ccea5
mtlpks <mtlpks@outlook.com>
2022-10-12 07:58:17 -0300

    Change to MySQL and create another table to interact with movie_list

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Movie Database Creator
 
-The purpose of this project is to train creating databases from websites through Python. The project itself takes the Top 250 Movies from IMDB, parses it through BeautifulSoup, creates the database using sqlite3 and displays it with pandas and tabulate
+The purpose of this project is to train creating databases from websites through Python. The project itself takes the Top 250 Movies from IMDB, parses it through BeautifulSoup, creates the database using mysql and displays it with pandas.
 
 So, we begin by using requests on the link provided. With the html file contents, we BeautifulSoup through it and use find_all to find our needed parts and append it to a list.
 Then, we create a loop that will use regex to find just the information we need, and create a dict entry with it.
 
-With the dict ready, what we have left is to create a database with our info. We create it through sqlite3, and then loop through the dict using a INSERT INTO statement to fill our database.
-Having used SELECT on the table, we display it in the screen by using pandas and tabulate.
+With the dict ready, what we have left is to create a database with our info. We create it through mysql, and then loop through the dict using a INSERT INTO statement to fill our database.
+Having used SELECT on the tables, we display it in the screen by using pandas.
 
 To run, simply download the files and run database_creator.py.
--- database_creator.py
+++ database_creator.py
@@ -1,24 +1,41 @@
-import sqlite3
+import mysql
 import pandas as pd
 import html_parsing
 from tabulate import tabulate
-conn = sqlite3.connect('movie_list')
+import mysql.connector
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Mla205766*"
+)
 cur = conn.cursor()
-cur.execute('''
-CREATE TABLE IF NOT EXISTS movie_list
-([movie_id] SERIAL PRIMARY KEY, [movie_rank] INTEGER, [director] TEXT, [rating] REAL, [year] INTEGER)
-''')
+cur.execute("""DROP DATABASE IF EXISTS imdb_top_movies""")
+cur.execute("""CREATE DATABASE imdb_top_movies""")
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Mla205766*",
+    database="imdb_top_movies"
+)
+curr = conn.cursor()
+curr.execute("""CREATE TABLE directors
+(id INT AUTO_INCREMENT PRIMARY KEY, director_name VARCHAR(50) NOT NULL UNIQUE)
+""")
+curr.execute(
+"""CREATE TABLE movie_list
+(movie_id INT AUTO_INCREMENT PRIMARY KEY, movie_rank INT, director_id INT, director_ph VARCHAR(55), title VARCHAR(55), year INT, INDEX fk_dir_id (director_id), FOREIGN KEY (director_id) REFERENCES directors(id))
+""")
+insert_movies = []
+insert_director = []
+insert_title = []
 for key, value in html_parsing.movies_list.items():
-    cur.execute('''
-    INSERT INTO movie_list(movie_rank, director, rating, year) 
-    VALUES(?, ?, ?, ?)
-    ''',(key, value[0], value[1], value[2]))
-cur.execute('''
-            SELECT
-            movie_rank, 
-            director, 
-            rating, 
-            year 
-            FROM movie_list''')
-df = pd.DataFrame(cur.fetchall(), columns=['movie_rank', 'director', 'rating', 'year'])
-print(tabulate(df, headers= 'keys', tablefmt= 'psql'))
+    insert_movies.append((key, value[0], value[1], value[2]))
+    insert_director.append((value[1],))
+insert_movies_query = """INSERT INTO movie_list(movie_rank, title, director_ph, year) VALUES (%s, %s, %s, %s)"""
+insert_director_query = """INSERT IGNORE INTO directors(director_name) VALUES (%s)"""
+curr.executemany(insert_movies_query, insert_movies)
+curr.executemany(insert_director_query, insert_director)
+curr.execute("""UPDATE movie_list INNER JOIN directors ON movie_list.director_ph = directors.director_name SET movie_list.director_id = directors.id""")
+df = pd.read_sql('SELECT movie_rank, title, year, directors.director_name FROM movie_list LEFT JOIN directors ON movie_list.director_id = directors.id', con=conn)
+df.columns = ['Movie Rank', 'Title', 'Year','Director']
+print(df.to_markdown())
